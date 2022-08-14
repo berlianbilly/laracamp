471cfd546d35e4e1a4fcbac1fb3ae72fa5504e1a
berlian ardiansyah <50651692+berlianbilly@users.noreply.github.com>
2022-08-14 20:10:38 +0700

    install breezee

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return Inertia::render('welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
 Route::get('login', function () {
     return view('login');
